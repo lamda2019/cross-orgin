c43fbae24949048ff728c595277e468c25b5211c
e <569188901@qq.com>
2019-05-09 17:12:31 +0800

    1. 浏览器端：发送AJAX请求前需设置通信对象XHR的withCredentials 属性为true。 2.服务器端：设置Access-Control-Allow-Credentials为true。

---
+++ origin-web/src/main/java/com/wsc/OriginWebApplication.java
@@ -0,0 +1,13 @@
+package com.wsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OriginWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OriginWebApplication.class, args);
+    }
+
+}
